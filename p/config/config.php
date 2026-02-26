2c2b57eee20f43f0e8ac67d2b0c1563450b21482
JakiGentho <zpasyadani@gmail.com>
2026-02-26 19:56:36 +0700

    Delete p directory

--- p/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('BASE_URL', 'http://localhost/perpusrlo/public/');
-define('SITE_NAME', 'Perpustakaan RLO');
-define('DENDA_PER_HARI', 1000);
-define('LAMA_PINJAM', 7);
-date_default_timezone_set('Asia/Jakarta');
-?>
-
--- p/src/views/layouts/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title ?? SITE_NAME; ?></title>
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/style.css">
-</head>
-<body>
